8c7a778e487f77849a04dbfbef00e8c16116bd62
Matěj Lukáč <m.lukac@netsystem.cz>
2026-06-04 12:51:59 +0200

    Changes to install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-/home/mates/Python/Cynoshure/.venv/bin/pyinstaller --onefile --name cyno main.py
-sudo cp dist/cyno /usr/local/bin/cyno
+path=$PWD
+$path/.venv/bin/pyinstaller --onefile --name cyno main.py
+sudo cp $path/dist/cyno /usr/local/bin/cyno
